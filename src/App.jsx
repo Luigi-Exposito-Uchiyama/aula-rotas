796e0eb375aafb9abc64fb75434ddd0c6e434e67
Luigi Uchiyama <luigi_uchiyama@outlook.com>
2023-09-05 21:31:43 -0300

    rodape adiciona no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import Rodape from '../components/Rodape';
 
 function App() {
   return (
     <>
       <Outlet />
+      <Rodape />
     </>
   );
 }
