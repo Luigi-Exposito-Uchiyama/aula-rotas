22e2364d488891031d158049a80be795eed75283
Luigi Uchiyama <luigi_uchiyama@outlook.com>
2023-09-05 21:41:34 -0300

    js adicionado

---
+++ src/components/produtos.js
@@ -0,0 +1,17 @@
+export const listaProdutos = [
+  {
+    id: 1,
+    nome: 'teclado',
+    preco: 150,
+  },
+  {
+    id: 2,
+    nome: 'mouse',
+    preco: 250,
+  },
+  {
+    id: 3,
+    nome: 'monitor',
+    preco: 350,
+  },
+];
