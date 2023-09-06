c055059961fd1d8a0fded7d9392d6580628dff4a
Luigi Uchiyama <luigi_uchiyama@outlook.com>
2023-09-05 21:19:56 -0300

    main atualizado

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,27 @@ import Produtos from '../routes/Produtos/index.jsx';
 import Error from '../routes/Erro/index.jsx';
 import EditarProduto from '../routes/EditarProdutos/index.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/produtos',
+        element: <Produtos />,
+      },
+      {
+        path: '/produtos/editar/:id',
+        element: <EditarProduto />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
